2cf35f8ee79a1d2a10983ea77066443defbc5b9f
Art <106353035+ArtemMikhalenia@users.noreply.github.com>
2023-04-27 23:54:38 +0300

    deleted some files

--- #source/js/script.js
+++ #source/js/script.js
@@ -9,16 +9,6 @@ if (iconMenu) {
 	});
 }
 
-function ibg() {
-	let ibg = document.querySelectorAll(".ibg");
-	for (var i = 0; i < ibg.length; i++) {
-		if (ibg[i].querySelector('img')) {
-			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
-		}
-	}
-}
-ibg();
-
 $(document).ready(function () {
 	$('.accessories__items').slick({
 		infinite: true,
